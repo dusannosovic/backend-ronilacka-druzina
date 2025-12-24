c4b857bcb07ddb88d5adf19cd3a3a4c7a6ee0d4c
dusannosovic <39407532+dusannosovic@users.noreply.github.com>
2025-12-24 16:24:07 +0100

    cors policy again

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://main.dtr7l15coknf0.amplifyapp.com/'],
+      origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://main.dtr7l15coknf0.amplifyapp.com'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnClick: true,
