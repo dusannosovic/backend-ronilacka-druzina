2b317fa2f67cf3e21ad1e1b9fdb100d8f87ee89c
dusannosovic <39407532+dusannosovic@users.noreply.github.com>
2025-12-26 10:37:02 +0100

    fix for cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,8 +4,9 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 
-        'https://main.dtr7l15coknf0.amplifyapp.com',   'https://www.ronilackadruzina.rs', 'https://api.ronilackadruzina.rs'],
+      origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://main.dtr7l15coknf0.amplifyapp.com', 
+        'https://ronilackadruzina.rs' ,   'https://www.ronilackadruzina.rs',
+        'https://api.ronilackadruzina.rs'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnClick: true,
